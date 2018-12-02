9f52bc6f0025d6f7413de75450605a646b8ca423
Evilham <github@evilham.com>
2018-12-02 11:22:02 +0100

    Typos.

--- adlermanager/IncidentManager.py
+++ adlermanager/IncidentManager.py
@@ -116,6 +116,6 @@ class IncidentManager(object):
 
     def component_status(self, component):
         return max((alert.status
-                    for alert in self.active_alerts.value()
+                    for alert in self.active_alerts.values()
                     if alert.labels.alertname == component),
                    default=Severity.OK)
