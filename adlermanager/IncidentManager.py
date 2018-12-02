d93eabc8ca6cb697204af5fbd95e565f4ab5b066
Evilham <github@evilham.com>
2018-12-02 05:59:36 +0100

    Fix writing files to yml

--- adlermanager/IncidentManager.py
+++ adlermanager/IncidentManager.py
@@ -1,7 +1,9 @@
 import attr
 import json
 import rfc3339
+import copy
 from datetime                import datetime
+from enum                    import IntEnum
 from munch                   import Munch
 from twisted.python.filepath import FilePath
 from twisted.internet        import reactor, defer, task
@@ -12,10 +14,10 @@ from .utils  import ensure_dirs, TimestampFile
 FILENAME_TIME_FORMAT = '%Y-%m-%d-%H%MZ'
 
 
-class Severity(object):
-    OK      = "0_OK"
-    WARNING = "1_WARNING"
-    ERROR   = "2_ERROR"
+class Severity(IntEnum):
+    OK      = 0
+    WARNING = 1
+    ERROR   = 2
 
     @classmethod
     def from_string(cls, s):
@@ -100,6 +102,9 @@ class IncidentManager(object):
     def log_event(self, message, timestamp, alerts=[]):
         obj = {"message": message, "timestamp": timestamp}
         if alerts:
+            alerts = copy.deepcopy(alerts)
+            for alert in alerts:
+                alert.status = alert.status.css
             obj["alerts"] = alerts
         self._logs.append(obj)
         # Persist messages
